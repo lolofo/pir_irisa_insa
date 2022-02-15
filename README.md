74f3d180fdc47b2017afe5d959e959d67b400ce5
lolofo <loicfosse65310@gmail.com>
2022-02-15 13:56:53 +0200

    test

---
+++ README.md
@@ -0,0 +1,31 @@
+# Research initiation project
+
+As part of my studies at INSA, in my 4th year I chose to do a research initiation project with the IRISA laboratory in Rennes. The theme of this project was to analyze (statistically) the behaviors of hidden embeddings on a BERT transformers architecture. We focused on 2 models that were trained on French lexicons (flauBERT and camemBERT).
+
+We wanted to compare the behavior of embeddings in two different situations:
+
+- First, on simply pre-trained architectures.
+
+- then on the same architectures but trained on a sentence classification task.
+
+
+## Table of contents
+
+1. [python_env](#pre_trained)
+2. [project 1](#fine_tuning)
+3. [project 2](#fine_tuned_models)
+
+
+
+## pre_trained
+
+In this section you will find a notebook which presents the analysis on the models called "Raw" that is to say not specified on a particular spot. These models are directly available on the Huggin face **transformers** library
+
+## fine_tuning
+
+In this section you will find the python scripts we used for the fine tuning of our models.
+We did a fine tuning of the FLUE/CLS data. 
+
+## fine_tuned_models
+
+
---
+++ fine_tuning/flue-cls-prepare.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import getopt
+import logging
+
+logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
+logger = logging.getLogger('flue-cls-finetune')
+
+checkpoint = 'flaubert/flaubert_base_cased'
+
+
+# ===================================================================
+#
+# define a number of constants we'll be using
+#
+def usage():
+    print('flue-cls-prepare.py [options] dir')
+    print('Synopsis:')
+    print('  Prepare data to be ready for finetuning given checkpoint and dump to dir')
+    print('Options:')
+    print('   -c, --checkpoint=s      set checkpoint to finetune (default: {})'.format(checkpoint))
+    print('   -h, --help              print this help')
+
+# ===================================================================
+#
+# parse command line
+#
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "checkpoint="])
+except getopt.GetoptError as err:
+    print(err) # will print something like "option -a not recognized"
+    sys.exit(1)
+
+for o, a in opts:
+    if o in ("-h", "--help"):
+        usage()
+        sys.exit(0)
+    elif o in ('-c', '--checkpoint'):
+        checkpoint = a
+    else:
+        assert False, "unhandled option {}".format(o)
+
+if len(args) != 1:
+    logging.error('Invalid number of arguments (use --help for usage)')
+    sys.exit(0)
+
+out = args[0]
+
+# ===================================================================
+#
+# now do the job
+#
+
+#
+# Faster and simpler way is to load directly from HuggingFace's dataset
+#
+
+logging.info('loading dataset')
+
+from datasets import load_dataset
+datasets = load_dataset("flue", "CLS")
+for split in datasets.keys():
+    logging.info('loaded {} entries from {} split'.format(len(datasets[split]), split))
+
+
+#
+# Load tokenizer
+#
+
+logging.info('loading tokenizer for {}'.format(checkpoint))
+
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+#
+# tokenize dataset
+#
+
+logging.info('tokenizing datasets')
+
+tokenized_datasets = datasets.map(lambda x: tokenizer(x['text'], padding="max_length", truncation=True), batched=True)
+
+
+#
+# tokenize dataset
+#
+
+logging.info('saving datasets to file {}'.format(out))
+
+tokenized_datasets.save_to_disk(out)
+
+logging.info("BYE!")
+
+
+
